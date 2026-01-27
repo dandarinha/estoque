31832601a60e2e5edb4220dedab915e648dfadee
dandarinha <dandarafrancabarbosa@gmail.com>
2026-01-27 13:13:08 -0300

    CRUD entrada e saida

---
+++ app/Http/Controllers/EntradaController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Entrada;
+use Illuminate\Http\Request;
+
+class EntradaController extends Controller
+{
+    public function store(Request $request)
+    {
+        $entrada = Entrada::create([
+            'id_produto' => $request->id_produto,
+            'quantidade' => $request->quantidade
+        ]);
+        return response()->json($entrada);
+    }
+
+    public function index()
+    {
+        $entradas = Entrada::all();
+        return response()->json($entradas);
+    }
+
+    public function delete($id)
+    {
+        $entrada = Entrada::find($id);
+
+        if ($entrada == null) {
+            return response()->json([
+                'erro' => 'Entrada não encontrada'
+            ]);
+        }
+        $entrada->delete();
+        return response()->json([
+            'mensagem' => 'Entrada excluída'
+        ]);
+    }
+}
