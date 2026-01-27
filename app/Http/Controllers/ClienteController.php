27d3124b789a802615993e17bbf09ba394aa2cea
dandarinha <dandarafrancabarbosa@gmail.com>
2026-01-27 10:14:53 -0300

    CRUD clientes

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -7,14 +7,22 @@ use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
-    public function store (Request $request)
+    public function store(Request $request)
     {
-        $cliente = Cliente::create([
-        'nome' => $request->nome,
-        'cpf' => $request->cpf,
-        'idade' => $request->idade,
-        ]);
-        return response()->json($cliente);
+        $clienteCadastrado = Cliente::where('cpf', '=', $request->cpf)->first();
+
+        if ($clienteCadastrado == null) {
+            $cliente = Cliente::create([
+                'nome' => $request->nome,
+                'cpf' => $request->cpf,
+                'idade' => $request->idade,
+            ]);
+            return response()->json($cliente);
+        } else {
+            return response()->json([
+                'erro' => 'cliente já cadastrado'
+            ]);
+        }
     }
 
     public function index()
@@ -30,18 +38,18 @@ class ClienteController extends Controller
             return response()->json([
                 'erro' => 'cliente não encontrado'
             ]);
-         $cpf = $request->cpf;
-        if($cpf == 'cpf'){
-            return response()->json([
-                'erro' => 'CPF já cadastrado'
-            ]);
-        }
+            $cpf = $request->cpf;
+            if ($cpf == 'cpf') {
+                return response()->json([
+                    'erro' => 'CPF já cadastrado'
+                ]);
+            }
         }
         $cliente->update();
 
         return response()->json([
             'mensagem' => 'cliente atualizado'
-            ]);
+        ]);
     }
 
     public function delete($id)
@@ -58,6 +66,4 @@ class ClienteController extends Controller
             'mensagem' => 'cliente excluído'
         ]);
     }
-
-
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\ProdutoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,3 +12,12 @@ Route::get('/produto',[ProdutoController::class, 'index'] );
 Route::put('/produto/{id}', [ProdutoController::class, 'update']);
 
 Route::delete('/produto/delete/{id}', [ProdutoController::class, 'delete']);
+
+
+Route::post('/cliente',[ClienteController::class, 'store'] );
+
+Route::get('/cliente',[ClienteController::class, 'index'] );
+
+Route::put('/cliente/{id}', [ClienteController::class, 'update']);
+
+Route::delete('/cliente/delete/{id}', [ClienteController::class, 'delete']);
